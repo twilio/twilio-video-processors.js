7c15023c42204e08f714d37e1123daf63968ec93
twilio-ci <twilio-ci@twilio.com>
2021-03-24 16:27:37 +0000

    1.0.0-rc2

---
+++ es5/index.d.ts
@@ -0,0 +1,17 @@
+import { BackgroundProcessor, BackgroundProcessorOptions } from './processors/background/BackgroundProcessor';
+import { GaussianBlurBackgroundProcessor, GaussianBlurBackgroundProcessorOptions } from './processors/background/GaussianBlurBackgroundProcessor';
+import { VirtualBackgroundProcessor, VirtualBackgroundProcessorOptions } from './processors/background/VirtualBackgroundProcessor';
+import { GrayscaleProcessor } from './processors/grayscale';
+import { Processor } from './processors/Processor';
+import { ImageFit, Dimensions } from './types';
+/**
+ * @private
+ */
+declare global {
+    interface Window {
+        Twilio: Object & {
+            VideoProcessors?: any;
+        };
+    }
+}
+export { BackgroundProcessor, BackgroundProcessorOptions, GaussianBlurBackgroundProcessor, GaussianBlurBackgroundProcessorOptions, GrayscaleProcessor, ImageFit, Processor, Dimensions, VirtualBackgroundProcessor, VirtualBackgroundProcessorOptions, };
---
+++ es5/processors/grayscale/index.js
@@ -0,0 +1,51 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GrayscaleProcessor = void 0;
+var Processor_1 = require("../Processor");
+/**
+ * @private
+ * The [[GrayscaleProcessor]] is a [[Processor]] which applies
+ * a grayscale transform to a frame.
+ */
+var GrayscaleProcessor = /** @class */ (function (_super) {
+    __extends(GrayscaleProcessor, _super);
+    function GrayscaleProcessor() {
+        var _this = _super.call(this) || this;
+        _this._outputFrame = new OffscreenCanvas(1, 1);
+        return _this;
+    }
+    /**
+     * Applies a grayscale transform to the input frame and generate an output frame.
+     * @param inputFrame - The input frame to process.
+     * @returns The outputframe or null if the transform cannot be applied.
+     */
+    GrayscaleProcessor.prototype.processFrame = function (inputFrame) {
+        this._outputFrame.width = inputFrame.width;
+        this._outputFrame.height = inputFrame.height;
+        var context = this._outputFrame.getContext('2d');
+        if (context) {
+            context.filter = 'grayscale(100%)';
+            context.drawImage(inputFrame, 0, 0, inputFrame.width, inputFrame.height);
+            return this._outputFrame;
+        }
+        return null;
+    };
+    return GrayscaleProcessor;
+}(Processor_1.Processor));
+exports.GrayscaleProcessor = GrayscaleProcessor;
+//# sourceMappingURL=index.js.map
