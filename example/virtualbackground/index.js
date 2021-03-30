ccb6fd00cb7c59726733dc0a5ba9ada57428dc3c
Joyce Ma <43423318+PikaJoyce@users.noreply.github.com>
2021-03-30 08:05:42 -0700

    VIDEO-4440 - GaussianBlur and VirtualBackground Example (#5)
    * saving blur background processor example progress
    
    * Working on Virtual Background, saving progress
    
    * Adding in checks
    
    * Adding RemoveProcessor button
    
    * Moving libraries into example
    
    * Adding remove processor button
    
    * Adding some more styling
    
    * Small fixes
    
    * Fixing styling and remove processor button
    
    * Adding Charlie's suggestions
    
    * Putting else block back in
    
    Co-authored-by: joma <joma@twilio.com>

---
+++ example/virtualbackground/index.js
@@ -0,0 +1,119 @@
+'use strict';
+
+const Video = Twilio.Video;
+const {GaussianBlurBackgroundProcessor, VirtualBackgroundProcessor} = Twilio.VideoProcessors;
+
+const gaussianBlurForm = document.querySelector('form#gaussianBlur-Form');
+const gaussianBlurButton = document.querySelector('button#gaussianBlur-Apply');
+const virtualBackgroundForm = document.querySelector('form#virtualBackground-Form');
+const virtualBackgroundButton = document.querySelector('button#virtualBackground-Apply');
+const videoInput = document.querySelector('video#video-input');
+const removeProcessorButton = document.querySelector('button#no-processor-apply');
+let videoTrack;
+let gaussianBlurProcessor;
+let virtualBackgroundProcessor;
+
+const loadImage = (name) =>
+  new Promise((resolve) => {
+    const image = new Image();
+    image.src = `backgrounds/${name}.jpg`;
+    image.onload = () => resolve(image);
+  });
+
+let images = {};
+Promise.all([
+  loadImage('breaking-news'),
+  loadImage('red-carpet'),
+  loadImage('windows-field'),
+]).then(([breakingNews, redCarpet, windowsField]) => {
+  images.breakingNews = breakingNews;
+  images.redCarpet = redCarpet;
+  images.windowsField = windowsField;
+  return images;
+});
+
+Video.createLocalVideoTrack().then((track) => {
+  track.attach(videoInput);
+  return videoTrack = track;
+});
+
+// Adding processor to Video Track
+const setProcessor = (processor, track) => {
+  if (track.processor) {
+    removeProcessorButton.disabled = true;
+    track.removeProcessor(track.processor);
+  }
+  if (processor) {
+    removeProcessorButton.disabled = false;
+    track.addProcessor(processor);
+  }
+};
+
+gaussianBlurButton.onclick = event => {
+  event.preventDefault();
+  const options = {};
+  let inferenceDimensions = {};
+  const inputs = gaussianBlurForm.getElementsByTagName('input');
+  for (let item of inputs) {
+    if (item.id === 'height' || item.id === 'width') {
+      item.valueAsNumber
+        ? (inferenceDimensions[item.id] = item.valueAsNumber)
+        : (inferenceDimensions[item.id] = 224);
+    } else {
+      options[item.id] = item.valueAsNumber;
+    }
+  }
+  const { maskBlurRadius, blurFilterRadius } = options;
+  if (!gaussianBlurProcessor) {
+    gaussianBlurProcessor = new GaussianBlurBackgroundProcessor({
+      inferenceDimensions,
+      maskBlurRadius,
+      blurFilterRadius,
+    });
+  } else {
+    gaussianBlurProcessor.inferenceDimensions = inferenceDimensions;
+    gaussianBlurProcessor.maskBlurRadius = maskBlurRadius;
+    gaussianBlurProcessor.blurFilterRadius = blurFilterRadius;
+  }
+  setProcessor(gaussianBlurProcessor, videoTrack);
+};
+
+virtualBackgroundButton.onclick = event => {
+  event.preventDefault();
+  const options = {};
+  let inferenceDimensions = {};
+  const inputs = virtualBackgroundForm.elements;
+  for (let item of inputs) {
+    if (item.id === 'height' || item.id === 'width') {
+      item.valueAsNumber
+        ? (inferenceDimensions[item.id] = item.valueAsNumber)
+        : (inferenceDimensions[item.id] = 224);
+    } else {
+      item.valueAsNumber
+        ? (options[item.id] = item.valueAsNumber)
+        : (options[item.id] = item.value);
+    }
+  }
+  let backgroundImage = images[options.backgroundImage];
+  let { maskBlurRadius, fitType } = options;
+  if (!virtualBackgroundProcessor) {
+    virtualBackgroundProcessor = new VirtualBackgroundProcessor({
+      inferenceDimensions,
+      maskBlurRadius,
+      backgroundImage,
+      fitType,
+    });
+  } else {
+    virtualBackgroundProcessor.backgroundImage = backgroundImage;
+    virtualBackgroundProcessor.inferenceDimensions = inferenceDimensions;
+    virtualBackgroundProcessor.fitType = fitType;
+    virtualBackgroundProcessor.maskBlurRadius = maskBlurRadius;
+  }
+  setProcessor(virtualBackgroundProcessor, videoTrack);
+};
+
+removeProcessorButton.disabled = true;
+removeProcessorButton.onclick = event => {
+  event.preventDefault();
+  setProcessor(null, videoTrack);
+};
