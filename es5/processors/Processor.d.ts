5057398a0c79c45798af622053398449ece35683
twilio-ci <twilio-ci@twilio.com>
2021-04-15 19:31:06 +0000

    1.0.0-rc3

---
+++ es5/processors/Processor.d.ts
@@ -0,0 +1,12 @@
+/**
+ * @private
+ * The [[Processor]] is an abstract class for building your own custom processors.
+ */
+export declare abstract class Processor {
+    /**
+     * Applies a transform to the input frame and generate an output frame.
+     * The frame will be dropped if this method returns null or raises and exception.
+     * @param inputFrame - The input frame to process.
+     */
+    abstract processFrame(inputFrame: OffscreenCanvas): Promise<OffscreenCanvas | null> | OffscreenCanvas | null;
+}
