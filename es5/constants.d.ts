3e178128de1e5e960bc8fbf1d02eee2ac28e4e20
twilio-ci <twilio-ci@twilio.com>
2021-04-16 15:20:47 +0000

    1.0.0-dev

--- es5/constants.d.ts
+++
@@ -1,8 +0,0 @@
-import { ModelConfig, PersonInferenceConfig } from '@tensorflow-models/body-pix/dist/body_pix_model';
-import { Dimensions } from './types';
-export declare const BLUR_FILTER_RADIUS = 5;
-export declare const MASK_BLUR_RADIUS = 3;
-export declare const HISTORY_COUNT = 5;
-export declare const MODEL_CONFIG: ModelConfig;
-export declare const INFERENCE_CONFIG: PersonInferenceConfig;
-export declare const INFERENCE_DIMENSIONS: Dimensions;
