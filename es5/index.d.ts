d85bdbcf37610cbb5301256467d3135ce207d426
twilio-ci <twilio-ci@twilio.com>
2021-05-28 22:09:22 +0000

    1.0.0-dev

--- es5/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { BackgroundProcessor, BackgroundProcessorOptions } from './processors/background/BackgroundProcessor';
-import { GaussianBlurBackgroundProcessor, GaussianBlurBackgroundProcessorOptions } from './processors/background/GaussianBlurBackgroundProcessor';
-import { VirtualBackgroundProcessor, VirtualBackgroundProcessorOptions } from './processors/background/VirtualBackgroundProcessor';
-import { GrayscaleProcessor } from './processors/grayscale';
-import { Processor } from './processors/Processor';
-import { ImageFit, Dimensions } from './types';
-export { BackgroundProcessor, BackgroundProcessorOptions, GaussianBlurBackgroundProcessor, GaussianBlurBackgroundProcessorOptions, GrayscaleProcessor, ImageFit, Processor, Dimensions, VirtualBackgroundProcessor, VirtualBackgroundProcessorOptions, };
