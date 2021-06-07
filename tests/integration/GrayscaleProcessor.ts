295234179246527920e34f353e57acbe0616f0b4
charliesantos <csantos@twilio.com>
2021-06-07 16:04:31 -0700

    Fix integration tests

--- tests/integration/GrayscaleProcessor.ts
+++ tests/integration/GrayscaleProcessor.ts
@@ -2,10 +2,10 @@ import * as assert from 'assert';
 import { GrayscaleProcessor } from '../../lib/processors/grayscale';
 
 describe('GrayscaleProcessor', () => {
-  it('should return an outputFrame', () => {
+  it('should process frame without an exception', () => {
     const processor = new GrayscaleProcessor();
-    const testFrame = new OffscreenCanvas(1, 1);
-    const frame = processor.processFrame(testFrame);
-    assert(!!frame);
+    const testFrame = new OffscreenCanvas(2, 2);
+    const canvas = document.createElement('canvas');
+    processor.processFrame(testFrame, canvas);
   });
 });
