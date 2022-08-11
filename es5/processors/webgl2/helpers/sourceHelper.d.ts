df121873978b3bd8dbbe105462ac2290fa7a0d10
twilio-ci <twilio-ci@twilio.com>
2022-08-11 22:01:08 +0000

    2.0.0-dev

--- es5/processors/webgl2/helpers/sourceHelper.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type SourceConfig = {
-    type: 'image' | 'video' | 'camera';
-    url?: string;
-};
-export declare type SourcePlayback = {
-    htmlElement: HTMLImageElement | HTMLVideoElement;
-    width: number;
-    height: number;
-};
