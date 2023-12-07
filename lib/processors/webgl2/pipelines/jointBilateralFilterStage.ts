6e73229f489a6b21bd2e09aa05277f60c425e783
mmalavalli <mmalavalli@twilio.com>
2023-12-07 11:07:20 -0600

    VIDEO-12963 Check if the person mask's edge passes through the kernel space.

--- lib/processors/webgl2/pipelines/jointBilateralFilterStage.ts
+++ lib/processors/webgl2/pipelines/jointBilateralFilterStage.ts
@@ -43,20 +43,27 @@ export function buildJointBilateralFilterStage(
     void main() {
       vec2 centerCoord = v_texCoord;
       vec3 centerColor = texture(u_inputFrame, centerCoord).rgb;
-      float centerSegAlpha = texture(u_segmentationMask, centerCoord).a;
       float newVal = 0.0;
 
       float spaceWeight = 0.0;
       float colorWeight = 0.0;
       float totalWeight = 0.0;
 
-      vec2 rightCoord = vec2(centerCoord + vec2(u_radius, 0) * u_texelSize);
-      vec2 bottomCoord = vec2(centerCoord + vec2(0, u_radius) * u_texelSize);
-      float rightSegAlpha = texture(u_segmentationMask, rightCoord).a;
-      float bottomSegAlpha = texture(u_segmentationMask, bottomCoord).a;
+      vec2 leftTopCoord = vec2(centerCoord + vec2(-u_radius, -u_radius) * u_texelSize);
+      vec2 rightTopCoord = vec2(centerCoord + vec2(u_radius, -u_radius) * u_texelSize);
+      vec2 leftBottomCoord = vec2(centerCoord + vec2(-u_radius, u_radius) * u_texelSize);
+      vec2 rightBottomCoord = vec2(centerCoord + vec2(u_radius, u_radius) * u_texelSize);
 
-      if (centerSegAlpha == rightSegAlpha && centerSegAlpha == bottomSegAlpha) {
-        outColor = vec4(vec3(0.0), centerSegAlpha);
+      float leftTopSegAlpha = texture(u_segmentationMask, leftTopCoord).a;
+      float rightTopSegAlpha = texture(u_segmentationMask, rightTopCoord).a;
+      float leftBottomSegAlpha = texture(u_segmentationMask, leftBottomCoord).a;
+      float rightBottomSegAlpha = texture(u_segmentationMask, rightBottomCoord).a;
+      float totalSegAlpha = leftTopSegAlpha + rightTopSegAlpha + leftBottomSegAlpha + rightBottomSegAlpha;
+
+      if (totalSegAlpha <= 0.0) {
+        outColor = vec4(vec3(0.0), 0.0);
+      } else if (totalSegAlpha >= 4.0) {
+        outColor = vec4(vec3(0.0), 1.0);
       } else {
         for (float i = -u_radius + u_offset; i <= u_radius; i += u_step) {
           for (float j = -u_radius + u_offset; j <= u_radius; j += u_step) {
