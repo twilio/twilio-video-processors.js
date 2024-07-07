f23709b3893541e079d97bbb8e1dd164c81d4bd6
twilio-ci <twilio-ci@twilio.com>
2024-07-07 19:50:14 +0000

    2.2.0-dev

--- es5/processors/webgl2/helpers/segmentationHelper.d.ts
+++
@@ -1,7 +0,0 @@
-export type InputResolution = '640x360' | '256x256' | '256x144' | '160x96' | string;
-export declare const inputResolutions: {
-    [resolution in InputResolution]: [number, number];
-};
-export type SegmentationConfig = {
-    inputResolution: InputResolution;
-};
--- es5/processors/webgl2/pipelines/loadSegmentationStage.d.ts
+++
@@ -1,5 +0,0 @@
-import { SegmentationConfig } from '../helpers/segmentationHelper';
-export declare function buildLoadSegmentationStage(gl: WebGL2RenderingContext, vertexShader: WebGLShader, positionBuffer: WebGLBuffer, texCoordBuffer: WebGLBuffer, segmentationConfig: SegmentationConfig, outputTexture: WebGLTexture): {
-    render: (segmentationData: Uint8ClampedArray) => void;
-    cleanUp: () => void;
-};
