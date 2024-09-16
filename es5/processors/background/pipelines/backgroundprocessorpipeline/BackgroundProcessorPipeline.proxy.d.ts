69ba3f443d7c92e7b409156cbfd28971e2cafb5a
twilio-ci <twilio-ci@twilio.com>
2024-09-16 20:19:30 +0000

    3.0.0-preview.2

---
+++ es5/processors/background/pipelines/backgroundprocessorpipeline/BackgroundProcessorPipeline.proxy.d.ts
@@ -0,0 +1,14 @@
+import { Remote } from 'comlink';
+import { BackgroundProcessorPipeline } from './BackgroundProcessorPipeline';
+/**
+ * @private
+ */
+export declare class BackgroundProcessorPipelineProxy {
+    protected readonly _pipelineWorkerPromise: Promise<Remote<BackgroundProcessorPipeline>>;
+    private readonly _benchmark;
+    protected constructor(pipelineWorkerPromise: Promise<Remote<BackgroundProcessorPipeline>>);
+    loadTwilioTFLite(): Promise<boolean>;
+    render(inputFrame: VideoFrame): Promise<ImageBitmap | null>;
+    setDeferInputFrameDownscale(defer: boolean): Promise<void>;
+    setMaskBlurRadius(radius: number): Promise<void>;
+}
