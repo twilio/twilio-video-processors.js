3545ad1ecc59f70d3f12f6a2bcb38183bcad66db
twilio-ci <twilio-ci@twilio.com>
2024-12-04 22:19:06 +0000

    3.0.0-dev

--- es5/processors/pipelines/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WebGL2Pipeline = exports.Pipeline = void 0;
-var Pipeline_1 = require("./Pipeline");
-Object.defineProperty(exports, "Pipeline", { enumerable: true, get: function () { return Pipeline_1.Pipeline; } });
-var webgl2pipeline_1 = require("./webgl2pipeline");
-Object.defineProperty(exports, "WebGL2Pipeline", { enumerable: true, get: function () { return webgl2pipeline_1.WebGL2Pipeline; } });
-//# sourceMappingURL=index.js.map
