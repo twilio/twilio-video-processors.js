854d124214afcceed6ff65e6842309d3eec7bc63
twilio-ci <twilio-ci@twilio.com>
2025-01-16 15:19:26 +0000

    3.0.1-dev

--- es5/processors/background/pipelines/backgroundprocessorpipeline/VirtualBackgroundProcessorPipeline.worker.d.ts
+++
@@ -1,7 +0,0 @@
-import { VirtualBackgroundProcessorPipeline } from './VirtualBackgroundProcessorPipeline';
-/**
- * @private
- */
-export declare class VirtualBackgroundProcessorPipelineWorker extends VirtualBackgroundProcessorPipeline {
-    render(inputFrame: VideoFrame | ImageBitmap): Promise<ImageBitmap | null>;
-}
