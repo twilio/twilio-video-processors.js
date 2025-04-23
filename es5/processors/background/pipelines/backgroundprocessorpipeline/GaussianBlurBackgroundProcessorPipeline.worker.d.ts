c605caf271cd610c337919f99e34d6004fbda028
twilio-ci <twilio-ci@twilio.com>
2025-04-23 15:42:23 +0000

    3.1.0-dev

--- es5/processors/background/pipelines/backgroundprocessorpipeline/GaussianBlurBackgroundProcessorPipeline.worker.d.ts
+++
@@ -1,7 +0,0 @@
-import { GaussianBlurBackgroundProcessorPipeline } from './GaussianBlurBackgroundProcessorPipeline';
-/**
- * @private
- */
-export declare class GaussianBlurBackgroundProcessorPipelineWorker extends GaussianBlurBackgroundProcessorPipeline {
-    render(inputFrame: VideoFrame | ImageBitmap): Promise<ImageBitmap | null>;
-}
