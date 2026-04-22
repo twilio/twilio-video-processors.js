4066f8451e36809a914d9aaa39c4ff32c8b4ecd8
twilio-ci <twilio-ci@twilio.com>
2026-04-22 18:33:17 +0000

    3.2.1-dev

--- es5/processors/background/pipelines/gaussianblurfilterpipeline/SinglePassGaussianBlurFilterStage.d.ts
+++
@@ -1,8 +0,0 @@
-import { WebGL2Pipeline } from '../../../pipelines';
-/**
- * @private
- */
-export declare class SinglePassGaussianBlurFilterStage extends WebGL2Pipeline.ProcessingStage {
-    constructor(glOut: WebGL2RenderingContext, direction: 'horizontal' | 'vertical', outputType: 'canvas' | 'texture', inputTextureUnit: number, outputTextureUnit?: number);
-    updateRadius(radius: number): void;
-}
--- es5/processors/background/pipelines/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { GaussianBlurFilterPipeline } from './gaussianblurfilterpipeline';
-export { PersonMaskUpscalePipeline } from './personmaskupscalepipeline';
